cdab5b69877184eb221fad5dee4508dcb9821183
anitha <anitha.s@ellucian.com>
2018-05-01 16:04:24 +0530

    version changes--Added test case

---
+++ src/com/example/test/helloworld.java
@@ -0,0 +1,13 @@
+package com.example.test;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class helloworld {
+
+    @Test
+    public void testAdd() {
+        Assert.assertEquals("Helloworld","Helloworld");
+    }
+}
+
